3eafcc6f467f390de12edd29a7b179434cd5a770
Romano Lynch <lynch@floydhamilton.nl>
2025-08-07 21:43:54 +0200

    From RLPL to a RPPL!

--- repl/repl.go
+++ repl/repl.go
@@ -3,9 +3,11 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/xPoppa/interpreter/lexer"
-	"github.com/xPoppa/interpreter/token"
 	"io"
+
+	"github.com/xPoppa/interpreter/lexer"
+	"github.com/xPoppa/interpreter/parser"
+	"github.com/xPoppa/interpreter/token"
 )
 
 const PROMPT = ">> "
@@ -22,9 +24,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
