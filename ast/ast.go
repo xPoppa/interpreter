85fb1fd4be82187680f0a12e40270eed7b90153f
Romano Lynch <lynch@floydhamilton.nl>
2025-08-07 20:17:39 +0200

    Adds CallExpression to ast
    Squash me with ast commit

--- ast/ast.go
+++ ast/ast.go
@@ -254,3 +254,27 @@ func (f *FunctionLiteral) String() string {
 
 	return out.String()
 }
+
+type CallExpression struct {
+	Token     token.Token // The '(' token
+	Function  Expression  // The functions identifier
+	Arguments []Expression
+}
+
+func (ce CallExpression) expressionNode()      {}
+func (ce CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, arg := range ce.Arguments {
+		args = append(args, arg.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
