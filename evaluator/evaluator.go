af8c16a6c79049db655b31e7f63c5f2cd702a10d
Romano Lynch <lynch@floydhamilton.nl>
2025-08-09 19:02:13 +0200

    Adds support for if else expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,9 +31,26 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
@@ -120,6 +137,19 @@ func evalMinusPrefixOperatorExpression(obj object.Object) object.Object {
 
 	value := obj.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
