1819ed2f8c600c40bce7279bd34b0309592725f5
Romano Lynch <lynch@floydhamilton.nl>
2025-08-09 15:12:50 +0200

    Adds evaluators for booleans and integers

---
+++ evaluator/evaluator.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+	"github.com/xPoppa/interpreter/ast"
+	"github.com/xPoppa/interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.BooleanExpression:
+		return &object.Boolean{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
