a164d412ac1a1973d44d1f3fbc9b51b698d06835
Romano Lynch <lynch@floydhamilton.nl>
2025-08-09 15:13:08 +0200

    Make it a REPL! :D

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/xPoppa/interpreter/evaluator"
 	"github.com/xPoppa/interpreter/lexer"
 	"github.com/xPoppa/interpreter/parser"
 )
@@ -30,8 +31,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
