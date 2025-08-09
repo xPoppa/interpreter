35999ae5eb827760d4a583d94784bc5941c66168
Romano Lynch <lynch@floydhamilton.nl>
2025-08-09 23:21:40 +0200

    Adds Error object

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
 	BOOLEAN_OBJ      ObjectType = "BOOLEAN"
 	NULL_OBJ         ObjectType = "NULL"
 	RETURN_VALUE_OBJ ObjectType = "RETURN"
+	ERROR_OBJ        ObjectType = "ERROR"
 )
 
 type Object interface {
@@ -41,3 +42,10 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
