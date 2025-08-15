8da4f8ee88354f96dc1f891184802c7799b58db7
Romano Lynch <lynch@floydhamilton.nl>
2025-08-15 22:42:58 +0200

    Adds function object

--- repl/repl.go
+++ repl/repl.go
@@ -35,7 +35,6 @@ func Start(in io.Reader, out io.Writer) {
 
 		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
-
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
 		}
