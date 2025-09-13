b9d90383c943695ada0faa5cb013456fbe29e021
Romano Lynch <lynch@floydhamilton.nl>
2025-09-13 14:49:34 +0200

    Finish parsing of arrays

--- parser/parser.go
+++ parser/parser.go
@@ -23,6 +23,7 @@ const (
 	PRODUCT     // *
 	PREFIX      // -X or !X
 	CALL        // myFunction(X)
+	INDEX       // array[index]
 )
 
 var precedences = map[token.TokenType]int{
@@ -35,6 +36,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type Parser struct {
@@ -76,6 +78,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 
 	// I do this to fill currToken and peekToken. [1,2,3,4] I think? Don't understand yet...
 	p.nextToken() // curroken = nothing && peekToken = 1
@@ -386,6 +389,19 @@ func (p *Parser) parseStringLiteral() ast.Expression {
 	literal := &ast.StringLiteral{Token: p.curToken, Value: p.curToken.Literal}
 	return literal
 
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+
+	return exp
 }
 
 // helpers
