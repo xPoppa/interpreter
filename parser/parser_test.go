680b6ceac6171da729343e29e576f4d687f3d393
Romano Lynch <lynch@floydhamilton.nl>
2025-09-13 14:39:37 +0200

    Adds ast node for index expression and add tests for parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -380,6 +380,14 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			"add(a + b + c * d / f + g)",
 			"add((((a + b) + ((c * d) / f)) + g))",
 		},
+		{
+			"a * [1, 2, 3, 4][b * c] * d",
+			"((a * ([1, 2, 3, 4][(b * c)])) * d)",
+		},
+		{
+			"add(a * b[2], b[1], 2 * [1, 2][1])",
+			"add((a * (b[2])), (b[1]), (2 * ([1, 2][1])))",
+		},
 	}
 	for _, tt := range tests {
 		l := lexer.New(tt.input)
@@ -836,4 +844,23 @@ func TestArrayLiteralParsing(t *testing.T) {
 		t.Errorf("Couldn't parse third element")
 	}
 
+}
+
+func TestParsingIndexExpression(t *testing.T) {
+	input := "myArray[1 + 1]"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	indexExp, ok := stmt.Expression.(*ast.IndexExpression)
+	if !ok {
+		t.Fatalf("exp not *ast.IndexExpression. got=%T", stmt.Expression)
+	}
+	if !testIdentifier(t, indexExp.Left, "myArray") {
+		t.Errorf("identifier is not myArray got=%s", indexExp.Left.String())
+	}
+	if !testInfixExpression(t, indexExp.Index, 1, "+", 1) {
+		t.Errorf("infix expression is not (1 + 1) got=%s", indexExp.Index.String())
+	}
 }
