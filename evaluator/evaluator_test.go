551454375159cfdaa92ae0fccb89bc989351ac6f
Romano Lynch <lynch@floydhamilton.nl>
2025-09-13 10:51:08 +0200

    Add string concat test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -374,5 +374,17 @@ func TestBuiltinFunctions(t *testing.T) {
 			}
 
 		}
+
+	}
+}
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
 	}
 }
