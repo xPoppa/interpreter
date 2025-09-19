b5c93289617ef3df0aeb461791e8bd360c9f0160
Romano Lynch <lynch@floydhamilton.nl>
2025-09-19 23:22:08 +0200

    Adds hashmaps to lexer and colon to tokenizer

--- ast/ast.go
+++ ast/ast.go
@@ -327,3 +327,23 @@ func (ie *IndexExpression) String() string {
 	out.WriteString("])")
 	return out.String()
 }
+
+type HashLiteral struct {
+	Token token.Token // the '{' token
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode()      {}
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, val := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+val.String())
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
