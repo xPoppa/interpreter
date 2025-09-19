c522da963091660316a9981e84a2f2cae878c68e
Romano Lynch <lynch@floydhamilton.nl>
2025-09-19 23:23:09 +0200

    Add hashkey type for o(1) lookup hashmaps
    I hash the string as a lookup key. And I use the values of the other 2
    objects as the key to the hashmap so that i can more easily lookup
    values from a map with the result of a hashfunction as the key :D

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/xPoppa/interpreter/ast"
@@ -28,6 +29,36 @@ type Object interface {
 }
 
 type BuiltinFunction func(args ...Object) Object
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+
+}
 
 type Integer struct {
 	Value int64
